6c3072f1c3ec45a1312b4bd4b2615b6344a15ada
Steelwix <mhunmael@hotmail.com>
2022-12-29 14:14:32 +0100

    fix issue on double create user route

--- src/Form/CreateUserFormType.php
+++ src/Form/CreateUserFormType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
