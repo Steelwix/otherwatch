b695ab0239476054365ec4bd9b413ae9cdc4c896
Steelwix <mhunmael@hotmail.com>
2023-01-04 13:43:49 +0100

    spell icon is now in MediasManager

--- src/Controller/AbilitiesController.php
+++ src/Controller/AbilitiesController.php
@@ -6,6 +6,7 @@ use App\Entity\Abilities;
 use App\Entity\SpellsIcons;
 use App\Form\CreateAbilityFormType;
 use App\Repository\AbilitiesRepository;
+use App\Service\MediasManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,22 +18,15 @@ class AbilitiesController extends AbstractController
 {
     #[Route('/create/ability', name: 'app_new_ability')]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour intéragir avec cette route')]
-    public function createAbility(AbilitiesRepository $abilitiesRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function createAbility(MediasManager $mediasManager, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $abilities = new Abilities;
-        $form = $this->createForm(CreateAbilityFormType::class, $abilities);
+        $ability = new Abilities;
+        $form = $this->createForm(CreateAbilityFormType::class, $ability);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid()) {
-            $spellsIcons = $form->get('spellsIcons')->getData();
-            $spellsName = md5(uniqid()) . '.' . $spellsIcons->guessExtension();
-            $spellsIcons->move(
-                $this->getParameter('spells_icons_directory'),
-                $spellsName
-            );
-            $newSpellsIcons = new SpellsIcons();
-            $newSpellsIcons->setName($spellsName);
-            $abilities->setSpellsIcons($newSpellsIcons);
-            $entityManager->persist($abilities);
+            $mediasManager->newSpellIcon($form->get('spellsIcons')->getData(), $ability);
+
+            $entityManager->persist($ability);
             $entityManager->flush();
             return $this->redirectToRoute('app_home');
         }
@@ -40,29 +34,20 @@ class AbilitiesController extends AbstractController
         return $this->render(
             'abilities/create_abilities.html.twig',
             [
-                'abilityForm' => $form->createView(), 'abilities' => $abilities
+                'abilityForm' => $form->createView(), 'abilities' => $ability
             ]
         );
     }
     #[Route('/modify/ability/{id}', name: 'app_modify_ability')]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour intéragir avec cette route')]
-    public function modifyAbility(Abilities $ability, AbilitiesRepository $abilitiesRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function modifyAbility(Abilities $ability, MediasManager $mediasManager, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(CreateAbilityFormType::class, $ability);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid()) {
             $spellsIcons = $form->get('spellsIcons')->getData();
             if ($spellsIcons != null) {
-
-
-                $spellsName = md5(uniqid()) . '.' . $spellsIcons->guessExtension();
-                $spellsIcons->move(
-                    $this->getParameter('spells_icons_directory'),
-                    $spellsName
-                );
-                $newSpellsIcons = new SpellsIcons();
-                $newSpellsIcons->setName($spellsName);
-                $ability->setSpellsIcons($newSpellsIcons);
+                $mediasManager->newSpellIcon($form->get('spellsIcons')->getData(), $ability);
             }
             $entityManager->persist($ability);
             $entityManager->flush();
