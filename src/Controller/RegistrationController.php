a3b6540aec305c43232da5743cdec7302f720fe0
Steelwix <mhunmael@hotmail.com>
2023-01-10 22:50:11 +0100

    Registration Controller now use FormHandler

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\CreateUserFormType;
+use App\Form\Handler\RegistrationFormHandler;
 use App\Form\RegistrationFormType;
 use App\Service\MediasManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,21 +17,14 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/create/user', name: 'app_create_user')]
-    public function createUser(UserPasswordHasherInterface $passwordHasher, Request $request, MediasManager $mediasManager, EntityManagerInterface $entityManager)
+    public function createUser(RegistrationFormHandler $registrationFormHandler, UserPasswordHasherInterface $passwordHasher, Request $request, MediasManager $mediasManager, EntityManagerInterface $entityManager)
     {
 
         $users = new Users;
         $form = $this->createForm(CreateUserFormType::class, $users);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid()) {
-            $mediasManager->newProfilePicture($form->get('profilePicture')->getData(), $users);
-            $hashedPassword = $passwordHasher->hashPassword(
-                $users,
-                $form->get('password')->getData()
-            );
-            $users->setPassword($hashedPassword);
-            $entityManager->persist($users);
-            $entityManager->flush();
+            $registrationFormHandler->handle($mediasManager, $entityManager, $form, $users, $passwordHasher);
             return $this->redirectToRoute('app_home');
         }
         return $this->render('user/create.html.twig', [
---
+++ src/Form/Handler/RegistrationFormHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form\Handler;
+
+use App\Entity\Abilities;
+use App\Entity\Heroes;
+use App\Entity\UpdateTicket;
+use App\Service\MediasManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Security\Core\Security;
+
+class RegistrationFormHandler
+{
+    public function handle($mediasManager, $entityManager, $form, $users, $passwordHasher)
+    {
+        $mediasManager->newProfilePicture($form->get('profilePicture')->getData(), $users);
+        $hashedPassword = $passwordHasher->hashPassword(
+            $users,
+            $form->get('password')->getData()
+        );
+        $users->setPassword($hashedPassword);
+        $entityManager->persist($users);
+        $entityManager->flush();
+    }
+}
